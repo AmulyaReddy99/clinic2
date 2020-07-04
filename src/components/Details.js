f4e20bc65ee08b3349d54d54cd6bab162c0b01a0
AmulyaReddy99 <amulyareddyk@yahoo.in>
2020-07-04 18:45:42 +0530

    hooks

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+function HookCounter(){
+    const [count, setCount] = useState(0)
+    const incFive = () => {
+        for(let i=0; i<5; i++){
+            setCount(prevCount => prevCount + 1)
+        }
+    }
+    return (
+        <React.Fragment>
+            <button onClick={()=>setCount(count+1)}>Current {count}</button>
+            <button onClick={()=>incFive(count+1)}>Inc 5 {count}</button>
+        </React.Fragment>
+    )
+}
 
 class Details extends React.Component{
+
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         count: 0
+    //     }
+    // }
+
+    // incrementCount = () => {
+    //     this.setState({
+    //         count: this.state.count+1
+    //     })
+    // }
+
     render(){
-        return "Details works!!"
+        return(
+        <div>
+            {/* <button onClick={this.incrementCount}>{this.state.count}</button> */}
+            <HookCounter></HookCounter>
+        </div>
+        );
     }
 }
 
