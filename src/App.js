aa9b7af6976fa22f9e225b2e10c883dd3a718a9b
AmulyaReddy99 <amulyareddyk@yahoo.in>
2020-07-04 12:19:33 +0530

    msg module func to class with props (app.js to msgs.js)

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 import { Navbar } from './components/Navbar';
 import CustomSidebar from './components/CustomSidebar';
-import { Messages, Members } from './components/Messages';
+import Messages from './components/Messages';
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.objs = [];
-    this.names = {
-      'C1': ['Hello.. Hw r ya?????? how r u doing.. hw is it in bangalore.. so long..!','cool','fine','how about hitachi?'],
-      'C2': ['I am fine..','cool','hw is jpmc','good'],
-      'C3': ['Happy birthday bro!'],
-      'C4': ['Ok'],
-      'C5': ['Met charan yesterday','nice']
-    };
-    for(var key in this.names) {
-      this.objs.push(<Members name={key} key={key}></Members>);
-    }
+  constructor(){
+    super();
   }
 
   render() {
@@ -31,14 +20,7 @@ class App extends Component {
             <CustomSidebar></CustomSidebar>
           </div>
           <div className="col-sm-9" style={{minHeight: '100vh'}}>
-            <div className="row">
-              <div className="col-sm-3" >
-                {this.objs}
-              </div>
-              <div className="col-sm-9">  
-                {/* {this.Messages} */}
-              </div>
-            </div>
+            <Messages></Messages>
           </div>
         </div>
       </div>
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,24 +1,47 @@
 import React from 'react';
 
-// class Messages extends React.Component{
-//     render(){
-//         return (
-//             <li></li>
-//         );
-//     }
-// }
+class Messages extends React.Component{
 
-const Members = props => {
-    return <p className="list">{props.name}</p>
-};
+    constructor(props){
+        super(props);
+        this.state = {
+            current_member:''
+        };
+        this.objs = [];
+        this.names = {
+            'C1': ['Hello.. Hw r ya?????? how r u doing.. hw is it in bangalore.. so long..!','cool','fine','how about hitachi?'],
+            'C2': ['I am fine..','cool','hw is jpmc','good'],
+            'C3': ['Happy birthday bro!'],
+            'C4': ['Ok'],
+            'C5': ['Met charan yesterday','nice']
+        };
+        for(var key in this.names) {
+            this.objs.push(<Members name={key} key={key}></Members>);
+        }
+    }
+    
+    changeMember(name){
+        this.setState({
+          current_member: name
+        })
+    }
 
-const Messages = props => {
-    return (
-        <p></p>
-    );
+    render(){
+        return (
+            <div className="row">
+              <div className="col-sm-3" >
+                {this.objs}
+              </div>
+              <div className="col-sm-9">  
+                {/* {this.Messages} */}
+              </div>
+            </div>
+        );
+    }
 }
 
-export{
-    Members,
-    Messages
+const Members = props => {
+    return <p className="list" onClick={()=>Messages.changeMember(props.name)}>{props.name}</p>
 };
+
+export default Messages;
